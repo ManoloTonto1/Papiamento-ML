10500fc768b876282e930b6f8da0deabe9e5fd57
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-04-18 15:04:13 +0200

    create pkl models

--- src/pdf-to-txt.py
+++ src/pdf-to-txt.py
@@ -2,14 +2,22 @@ import fitz  # this is pymupdf
 import os
 path = "./downloads"
 text = ""
-for filename in os.listdir(path):
-    f = os.path.join(path, filename)
-    # checking if it is a file
-    if os.path.isfile(f):
-        with fitz.open(f) as doc:
-            for page in doc:
-                text += page.get_text()
-                with open('data.txt', 'w',encoding="utf-8") as text_file:
-                    text_file.write(text)
+# for filename in os.listdir(path):
+#     f = os.path.join(path, filename)
+#     # checking if it is a file
+#     if os.path.isfile(f):
+#         with fitz.open(f) as doc:
+#             for page in doc:
+#                 text += page.get_text()
+#                 with open('data.txt', 'w',encoding="utf-8") as text_file:
+#                     text_file.write(text)
                 # print(text)
+
+file = "./downloads/BNA-DIG-PAP-VOCABULARIO.pdf"
+with fitz.open(file) as doc:
+    for page in doc:
+        text += page.get_text()
+        with open('papiamento_words.txt', 'w',encoding="utf-8") as text_file:
+            text_file.write(text)
+    # print(text)
 
