23868c9c60db05ff0ecc0b8770f6f30367eca8ce
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-04-24 21:40:06 +0200

    Update datashaver.py

--- src/datashaver.py
+++ src/datashaver.py
@@ -1,7 +1,17 @@
 import re
 
-my_file = open("data.txt", "w")
+filename = "data.txt"
 
-string = open(my_file).read()
-new_str = re.sub('[^A-Za-z0-9]+', '', string)
-open(my_file, 'w').write(new_str)
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, mode='rt', encoding='utf-8')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+
+print(load_doc(filename))
+# new_str = re.sub('[^A-Za-z0-9]+', '', string)
+# open(my_file, 'w').write(new_str)
