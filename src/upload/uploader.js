47edb58d6a92131bcc5e2c84a2c46904641d12cd
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-04-30 20:20:34 +0200

    Uploaded first batch and uploader working

--- src/upload/uploader.js
+++ src/upload/uploader.js
@@ -1,8 +1,27 @@
-//import {db} from './Firebase';
-const fs = require('fs');
+import {db} from './Firebase.js';
+import * as fs from 'fs';
+import {addDoc,collection} from "firebase/firestore";
 
-const filedir = './sample.txt';
-fs.readFile(filedir,(error, data)=>{
-    console.log(data.toString("utf-8"));
-})
 
+
+const filedir = './vocabulario.txt';
+fs.readFile(filedir, (error, data) => {
+    data.toString("utf-8").split("\n").forEach( async line => {
+        // Add a new document in collection 
+        await addDoc(collection(db, "Papiamento-data"), {
+            papiamento: line,
+            isNotPapiamento: 0,
+            isPapiamento: 0,
+            english:[],
+        });
+
+    });
+});
+
+// await addDoc(collection(db, "Papiamento-data"), {
+//     papiamento: "saki ta un palabra",
+//     isNotPapiamento: 0,
+//     isPapiamento: 0,
+//     english:[],
+// });
+console.log("all data has been uploaded, good luck and good job!")
